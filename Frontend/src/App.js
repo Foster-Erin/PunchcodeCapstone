cbb62971f61d68671a7b154ea43eb4e246cae8e3
Erin Foster <erint.foster@gmail.com>
2021-09-16 21:13:16 -0700

    Corrected Syntax Error

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -22,7 +22,7 @@ function handleParamChange(e) {
   return (
     <Container className="mb-4">
       <h1 className="mb-4">JR. DEV JOB SEARCH</h1>
-      <SearchForm params={params} onParamChange={handleParamChange}
+      <SearchForm params={params} onParamChange={handleParamChange}/>
       <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error. Try Refreshing.</h1>}
