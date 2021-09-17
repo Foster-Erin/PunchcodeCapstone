53d74c6d23325eb3be36c1029329172937634cd5
Erin Foster <erint.foster@gmail.com>
2021-09-16 19:50:15 -0700

    Show/Hide View Of Job Details

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,21 +1,22 @@
-import useFetchJobs from './useFetchJobs';
-import React, {useState} from 'react';
-import {Container} from 'react-bootstrap'; 
-import Job from './Job';
+import useFetchJobs from "./useFetchJobs";
+import React, { useState } from "react";
+import { Container } from "react-bootstrap";
+import Job from "./Job";
 
 function App() {
-  const [params, setParams]= useState({})
-  const [page, setPage]= useState(1)
-  const {jobs, loading, error} = useFetchJobs(params, page)
-  return (<Container>
-    {loading && <h1>Loading...</h1>}
-    {error && <h1>Error. Try Refreshing.</h1>}
-    {jobs.map(job => {
-      return <Job key={job.id} job={job}/>
-    })}
-  </Container>
-
-  )
+  const [params, setParams] = useState({});
+  const [page, setPage] = useState(1);
+  const { jobs, loading, error } = useFetchJobs(params, page);
+  return (
+    <Container className="mb-4">
+      <h1>JR. DEV JOB SEARCH</h1>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error. Try Refreshing.</h1>}
+      {jobs.map((job) => {
+        return <Job key={job.id} job={job} />;
+      })}
+    </Container>
+  );
 }
 
 export default App;
--- Frontend/src/Job.js
+++ Frontend/src/Job.js
@@ -1,32 +1,52 @@
-import React from 'react'
-import { Card, Badge } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Card, Badge, Button, Collapse } from "react-bootstrap";
 
 export default function Job({ job }) {
-    return (
-                   //import npm i react markdown to convert markdown to react code if necessary (after badges)//
-        <Card>
-            <div className="d-flex justify-content-between">
-                <div>   
-                   <Card.Title>
-                   {job.title} - <span className="text-muted font-weight-light">
-                       {job.company}</span> 
-                   </Card.Title> 
-                   <Card.Subtitle className="text-muted mb-2">
-                       {new Date(job.created_at).toLocaleDateString()}
-                   </Card.Subtitle>
-                   <Badge variant="secondary" className="mr-2">{job.type}</Badge>
-                   <Badge variant="secondary">{job.location}</Badge>
-                   <div style={{wordBreak: 'break-all'}}>
-                       <ReactMarkdown source={job.how_to_apply}/>
-                   </div>
-                </div>
-            <img className="d-none d-md-block" height="50" src={job.company_logo} alt={job.company}/>
-            
+  const [open, setOpen] = useState(false);
+  return (
+    //import npm i react markdown to convert markdown to react code if necessary (after badges)//
+    <Card className="mb-3">
+      <Card.Body>
+        <div className="d-flex justify-content-between">
+          <div>
+            <img
+              className="d-none d-md-block"
+              height="50"
+              src={job.company_logo}
+              alt={job.company}
+            />
+            <Card.Title>
+              {job.title} -{" "}
+              <span className="text-muted font-weight-light">
+                {job.company}
+              </span>
+            </Card.Title>
+            <Card.Subtitle className="text-muted mb-2">
+              {new Date(job.created_at).toLocaleDateString()}
+            </Card.Subtitle>
+            <Badge variant="secondary" className="mr-2">
+              {job.type}
+            </Badge>
+            <Badge variant="secondary">{job.location}</Badge>
+            <div style={{ wordBreak: "break-all" }}>
+              <ReactMarkdown source={job.how_to_apply} />
             </div>
-        <Card.Body>
-
-        </Card.Body>
-        </Card>
-        
-    )
+          </div>
+        </div>
+        <Card.Text>
+          <Button
+            onClick={() => setOpen((prevOpen) => !prevOpen)}
+            variant="primary"
+          >
+            {open ? "Hide Details" : "View Details"}
+          </Button>
+        </Card.Text>
+        <Collapse in={open}>
+          <div className="mt-4">
+            <ReactMarkdown source={job.description} />
+          </div>
+        </Collapse>
+      </Card.Body>
+    </Card>
+  );
 }
