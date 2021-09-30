f5de7fe4d59f9714e83a54502046f4c46ddc041c
Erin Foster <erint.foster@gmail.com>
2021-09-29 19:34:24 -0700

    Abandoned Grid/Flexed Job Cards

--- Frontend/src/Job.js
+++ Frontend/src/Job.js
@@ -1,37 +1,31 @@
-import React from 'react';
-import './Job.css';
-import './Glass.css';
+import React from "react";
+import "./Job.css";
+import "./Glass.css";
 
 export default function Job({ job }) {
   return (
-    <div className='cardBody grid glass'>
-      <div className='align-items-center'>
-        <div>
-          <div className='cardTitle'>
-            {job.title} -{' '}
-            <span className='font-weight-bold'>{job.company.display_name}</span>
-          </div>
-          <div className='cardSubtitle'>{job.salary_min}</div>
-          <div variant='secondary' className='badge'>
-            {' '}
-            {job.category.label}{' '}
-          </div>
-
-          <div variant='secondary' className='badge'>
-            <span class='badge bg-dark'></span> {job.location.area}{' '}
-          </div>
-          <div style={{ wordBreak: 'break-all' }}>{job.id.redirect_url}</div>
+    <div className="cardBody grid glass">
+      <div className="cardTitle">
+        {job.title} - <span>{job.company.display_name}</span></div>
+        <div className="cardSubtitle">{job.salary_min}
+        <div variant="secondary" className="badge">
+          {" "}
+          {job.category.label}{" "}
         </div>
+        <div variant="secondary" className="badge">
+          <span class="badge"></span> {job.location.area}{" "}
+        </div>
+        <div style={{ wordBreak: "break-all" }}>{job.id.redirect_url}</div>
       </div>
 
-      <div className='jobDescription'>{job.description}</div>
+      <div className="jobDescription">{job.description}</div>
       <a
         href={job.redirect_url}
-        target='_blank'
-        rel='noreferrer'
-        className='apply'
+        target="_blank"
+        rel="noreferrer"
+        className="apply"
       >
-        <button id='applyButton'>APPLY NOW</button>
+        <button id="applyButton">APPLY NOW</button>
       </a>
     </div>
   );
