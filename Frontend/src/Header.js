a847381f5202d430239f3d90b64f56d4c1e3c1b6
Ty G <tysongyorfy@gmail.com>
2021-09-29 17:38:58 -0700

    added logo back in

--- Frontend/src/Header.js
+++ Frontend/src/Header.js
@@ -1,19 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <header className="header">
-      <nav className="nav">
-        <h1>NEWBIE</h1>
-        <ul className="navLinks">
+    <header className='header'>
+      <nav className='nav'>
+        <img src='./Assets/logomouse2.png' alt='logo' />
+        <ul className='navLinks'>
           <li>
-            <Link to="/">HOME</Link>
+            <Link to='/'>HOME</Link>
           </li>
           <li>
-            <Link to="/login">LOGIN</Link>
+            <Link to='/login'>LOGIN</Link>
           </li>
         </ul>
-        <button type="button" className="contact">
+        <button type='button' className='contact'>
           LOGIN
         </button>
       </nav>
