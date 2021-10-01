711ed125e6c60b27b34e17fe0504e18697daad41
Erin Foster <erint.foster@gmail.com>
2021-10-01 11:54:50 -0700

    Updated Logo & Header/Footer Background

--- Frontend/src/LoginForm.js
+++ Frontend/src/LoginForm.js
@@ -3,18 +3,23 @@ import './LoginForm.css';
 
 function LoginForm(){
     return (
-        <section className="loginContainer">
+      <div className="cardBody grid glass">
+        {/* <section className="loginContainer"> */}
         <div class="login">
           <form action="#" method="get" id="login-form">
-          <label for="username" class="required" type="text">Username</label>
-          <input required type="text" name="username" class="inputs"/>
-          <label for="password" class="required" type="password" id="password">Password</label>
-          <input required type="text" name="password" class="inputs"/>
+          {/* <label for="username" class="required" type="text">Username</label> */}
+      
+          <input required type="text" name="username" class="inputs" placeholder="Username"/>
+          {/* <label for="password" class="required" type="password" id="password">Password</label> */}
+          <input required type="text" name="password" class="inputs" placeholder="Password" id="password"/>
           <button type="submit" value="submit" id="login-submit">LOGIN</button>
         </form>
         </div>
-      </section>
+      {/* </section> */}
+    </div>
     );
 }
+
+
 
 export default LoginForm;
